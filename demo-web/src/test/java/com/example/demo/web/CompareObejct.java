93c3dfe1d6fde323363030c9109754953e13ec2e
luyongfa <luyongfa@zhihuiguan.net>
2019-04-12 17:42:07 +0800

    compare

---
+++ demo-web/src/test/java/com/example/demo/web/CompareObejct.java
@@ -0,0 +1,63 @@
+package com.example.demo.web;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * 对比两个对象的差值
+ * Created by Administrator on 2018/7/9.
+ */
+public class CompareObejct<T> {
+
+    private T original;
+
+    private T current;
+
+    /**
+     * @param cls
+     * @return
+     */
+    public String contrastObj(Class<T> cls) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            Field[] fields = cls.getDeclaredFields();
+            for (Field field : fields) {
+                PropertyDescriptor pd = new PropertyDescriptor(field.getName(), cls);
+                Method getMethod = pd.getReadMethod();
+                String type = field.getType().getName();
+                if (!"java.util.Set".equals(type)) {
+                    Object o1 = getMethod.invoke(this.original);
+                    Object o2 = getMethod.invoke(this.current);
+                    if (null != o2) {
+                        String s1 = o1 == null ? "" : o1.toString();
+                        String s2 = o2 == null ? "" : o2.toString();
+                        if (!s1.equals(s2)) {
+                            sb.append(field.getName() + ":" + "[" + s1 + "," + s2 + "];");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+
+    public T getOriginal() {
+        return original;
+    }
+
+    public void setOriginal(T original) {
+        this.original = original;
+    }
+
+    public T getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(T current) {
+        this.current = current;
+    }
+}
---
+++ demo-web/src/test/java/com/example/demo/web/CompareObejctTest.java
@@ -0,0 +1,11 @@
+package com.example.demo.web;
+
+import com.example.demo.common.entity.sys.SysUser;
+
+public class CompareObejctTest {
+
+    CompareObejct<Long> compareObejct = new CompareObejct<Long>();
+
+    CompareObejct<SysUser> comparer = new CompareObejct<SysUser>();
+
+}
